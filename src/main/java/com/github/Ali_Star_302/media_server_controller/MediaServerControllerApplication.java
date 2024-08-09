a573b40f17f5258da118d5397989d1124b2c7b8a
Alistair <AlistairJR302@Gmail.com>
2024-08-09 15:34:05 +0100

    Initial commit

---
+++ src/main/java/com/github/Ali_Star_302/media_server_controller/MediaServerControllerApplication.java
@@ -0,0 +1,13 @@
+package com.github.Ali_Star_302.media_server_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaServerControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaServerControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Ali_Star_302/media_server_controller/MediaServerControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Ali_Star_302.media_server_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServerControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
