729c2a6b6db7a24cbdb2dc137766af76a310f758
Alistair <AlistairJR302@Gmail.com>
2024-08-12 17:29:54 +0100

    Added test toggle for the media server button

--- src/main/java/com/github/Ali_Star_302/media_server_controller/control_panel/ControlPanelController.java
+++ src/main/java/com/github/Ali_Star_302/media_server_controller/control_panel/ControlPanelController.java
@@ -1,12 +1,28 @@
 package com.github.Ali_Star_302.media_server_controller.control_panel;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ControlPanelController {
+
+    private final ControlPanelService panelService;
+
+    @Autowired
+    public ControlPanelController(ControlPanelService panelService) {
+        this.panelService = panelService;
+    }
+
     @GetMapping("/control-panel")
     public String controlPanel() {
+        return "control-panel";
+    }
+
+    @PostMapping("/control-panel")
+    public String mediaServerBtnPress() {
+        panelService.toggleMediaServer();
         return "control-panel";
     }
 }
---
+++ src/main/java/com/github/Ali_Star_302/media_server_controller/control_panel/ControlPanelService.java
@@ -0,0 +1,11 @@
+package com.github.Ali_Star_302.media_server_controller.control_panel;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ControlPanelService {
+
+    public void toggleMediaServer() {
+        System.out.println("toggle media server");
+    }
+}
