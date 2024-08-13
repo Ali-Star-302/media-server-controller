6fa86d396db76f1601d1412392cd817850792f9f
Alistair <AlistairJR302@Gmail.com>
2024-08-13 18:35:05 +0100

    Removed application.properties

--- src/main/java/com/github/Ali_Star_302/media_server_controller/control_panel/ControlPanelController.java
+++ src/main/java/com/github/Ali_Star_302/media_server_controller/control_panel/ControlPanelController.java
@@ -2,6 +2,7 @@ package com.github.Ali_Star_302.media_server_controller.control_panel;
 
 import com.github.Ali_Star_302.media_server_controller.ping.PingServicer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,21 +13,25 @@ public class ControlPanelController {
 
     private final ControlPanelService panelService;
     private final PingServicer pingService;
+
+    @Value("${mediaServer.address.ip}")
     private final String mediaServerIp;
+
+    @Value("${mediaServer.address.mac}")
     private final String mediaServerMac;
 
     @Autowired
     public ControlPanelController(ControlPanelService panelService, PingServicer pingService) {
         this.panelService = panelService;
         this.pingService = pingService;
-        this.mediaServerIp = "192.168.0.0";
-        this.mediaServerMac = "8C-89-A5-E7-4D-4C";
-//        this.mediaServerIp = "127.0.0.1";
+        this.mediaServerIp = "";
+        this.mediaServerMac = "";
     }
 
     @GetMapping("/control-panel")
     public ModelAndView controlPanel() {
         ModelAndView modelAndView = new ModelAndView("control-panel");
+        System.out.println(mediaServerIp);
         modelAndView.addObject("awake", this.pingService.sendPing(mediaServerIp, 1000));
 
         return modelAndView;
@@ -36,6 +41,7 @@ public class ControlPanelController {
     public ModelAndView mediaServerBtnPress() {
         panelService.wakePc(mediaServerIp, mediaServerMac);
         ModelAndView modelAndView = new ModelAndView("control-panel");
+        System.out.println(mediaServerIp);
         modelAndView.addObject("awake", this.pingService.sendPing(mediaServerIp, 1000));
 
         return modelAndView;
